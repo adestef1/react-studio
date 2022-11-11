c6d94d53adb4eaa67bf4ba9e3217d3d469b12a84
adestef1 <60240814+adestef1@users.noreply.github.com>
2022-11-11 15:22:09 -0500

    Add files via upload

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+import React from "react";
+
+export default function BakeryItem(props) {
+    return (
+        <div className="bakeryItem">
+            <img src={props.item.image} alt="bakery item"></img>
+            <div className="details">
+                <div className="descr">
+                    <h1>{props.item.name}</h1>
+                    <p>{props.item.description}</p>
+                </div>
+                <div className="pricing">
+                    <p>${props.item.price}</p>
+                    <button onClick={() => props.updateCart(props.index)}>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
